7df89e96da04b9cd45797ab906b5c98245502c3b
Pete Farquhar <petefarq@gmail.com>
2019-10-23 14:31:06 -0400

    final revision

--- EmployeeSQL/schemata.sql
+++ EmployeeSQL/schemata.sql
@@ -1,26 +1,27 @@
 CREATE TABLE employees (
-emp_no INT PRIMARY KEY,
-birth_date DATE,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-gender VARCHAR(1),
-hire_date DATE
+emp_no INT PRIMARY KEY NOT NULL,
+birth_date DATE NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+gender VARCHAR(1) NOT NULL,
+hire_date DATE NOT NULL
 );
 
 SELECT * from employees;
 
 CREATE TABLE departments (
-dept_no VARCHAR(4) PRIMARY KEY,
-dept_name VARCHAR	
+dept_no VARCHAR(4) PRIMARY KEY NOT NULL,
+dept_name VARCHAR(30) NOT NULL	
 );
 
 SELECT * from departments;
 
 CREATE TABLE dept_emp (
-emp_no INT,
-dept_no VARCHAR(4),
-from_date DATE,
-to_date DATE,
+de_id SERIAL PRIMARY KEY NOT NULL,
+emp_no INT NOT NULL,
+dept_no VARCHAR(4) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
@@ -28,10 +29,11 @@ FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 SELECT * from dept_emp;
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4),
-emp_no INT,  DEpt
-from_date DATE,
-to_date DATE,
+dm_id SERIAL PRIMARY KEY NOT NULL,
+dept_no VARCHAR(4) NOT NULL,
+emp_no INT NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
@@ -39,20 +41,22 @@ FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 SELECT * from dept_manager;
 
 CREATE TABLE salaries (
-emp_no INT,
-salary INT,
-from_date DATE,
-to_date DATE,
+salary_id SERIAL PRIMARY KEY NOT NULL,
+emp_no INT NOT NULL,
+salary INT NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)	
 );
 
 SELECT * from salaries;
 
 CREATE TABLE titles (
-emp_no INT,
-title VARCHAR,
-from_date DATE,
-to_date DATE,
+title_id SERIAL PRIMARY KEY NOT NULL,
+emp_no INT NOT NULL,
+title VARCHAR(30) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)	
 );
 
